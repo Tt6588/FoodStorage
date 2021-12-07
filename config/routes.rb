4c681948b04336943c598d736e0c080a11ce0bd8
Tt6588 <tatsusato1011@gmail.com>
2021-12-07 04:45:51 +0000

    [add]routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,20 @@ Rails.application.routes.draw do
   
     root to: 'homes#top'
     get "/about", to: "homes#about"
+    
+    resources :users, only:[:show, :edit, :update, :out]
+    
+    # 一人用食材管理ルーティング
+    resources :foods, only:[:index, :create,:show, :edit, :update, :destroy]# 食材
+    resources :comments, only:[:create, :destroy]                           # 食材コメント
+    
+    # 複数人用食材管理ルーティング
+    resources :groups, only:[:index, :create, :destroy]                           # グループ機能
+    resources :share_foods, only:[:index, :create,:show, :edit, :update, :destroy]# 共有食材
+    resources :share_comments, only:[:create, :destroy]                           # 共有食材コメント
+    
+    # その他追加機能
+    resources :notices, only:[:index]                        # 通知一覧機能
+    resources :genres, only:[:index, :create, :edit, :update]# ジャンル機能
+    # 検索機能は他機能が完成後に設定
 end
