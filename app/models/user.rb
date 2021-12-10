c3ab5b93688067867d2d0f254127a4f435fc8323
Tt6588 <tatsusato1011@gmail.com>
2021-12-10 12:46:39 +0000

    [add]user--show+edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :notices, dependent: :destroy
   
+  attachment :profile_image #プロフィール画像用
+  
   #共有管理用アソシエーション
   has_many :group_users, dependent: :destroy
   # has_many :groups, through: :group_users
