4003156a25c08865a723f875d3a457ea44ae0b0e
Tt6588 <tatsusato1011@gmail.com>
2021-12-19 11:03:04 +0000

    [fix]更新時に登録した時のデータが反映されないエラーの改善

--- app/controllers/share_foods_controller.rb
+++ app/controllers/share_foods_controller.rb
@@ -1,5 +1,5 @@
 class ShareFoodsController < ApplicationController
-  
+
   before_action :authenticate_user!
     before_action :correct_share_food,only: [:edit, :show, :new, :index]
 
@@ -24,8 +24,8 @@ class ShareFoodsController < ApplicationController
     @group = Group.find(params[:group_id])
     @share_food_addition = ShareFood.where(group_id: @group.id)
     @share_foods_none = "食材を追加してください"
-    
-    #ソート
+
+    #ソート機能
     if params[:sort_update1]
       @share_foods = ShareFood.latest1
     elsif params[:sort_update2]
@@ -68,7 +68,7 @@ class ShareFoodsController < ApplicationController
       redirect_to "/groups/#{@group.id}/share_foods/#{@share_food.id}/edit", alert: '食材を削除出来ませんでした'
     end
   end
-  
+
   def correct_share_food
     group_id = params[:group_id].to_i
     group_user = GroupUser.find_by(user_id: current_user.id,  group_id: group_id)
