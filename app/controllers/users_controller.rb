8769d8951a6160666ccd6ded765c4c431044254b
Tt6588 <tatsusato1011@gmail.com>
2022-01-17 12:37:03 +0900

    [update]不要なモデル・コントローラの削除、user controller記述省略

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,20 +3,18 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :correct_user,only: [:edit]
   
+  before_action :user_find, only: [:show, :edit, :update]
+  
   def show
-    @user = User.find(params[:id])
-
     @group_lists = Group.all
     @group_joining = GroupUser.where(user_id: @user.id)
     @group_lists_none = "グループに参加していません"
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user.id), notice: '管理者情報を更新しました。'
     else
@@ -25,7 +23,7 @@ class UsersController < ApplicationController
   end
   
   def correct_user
-        @user = User.find(params[:id])
+    @user = User.find(params[:id])
     unless @user.id == current_user.id
       redirect_to user_path(@user.id)
     end
@@ -35,6 +33,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :email)
+  end
+  
+  def user_find
+    @user = User.find(params[:id])
   end
   
 end
--- app/models/genre.rb
+++
@@ -1,8 +0,0 @@
-class Genre < ApplicationRecord
-
-  # has_many :foods, dependent: :destroy
-
-  #共有管理用アソシエーション
-  # has_many :share_foods, dependent: :destroy
-
-end
